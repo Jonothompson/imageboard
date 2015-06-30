25c855217daa917c73526ad32c2d178c661eefd8
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-30 18:46:04 -0400

    Moved static data to route

--- app/components/pins-index.js
+++ app/components/pins-index.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'ul',
-	
-	pins: [
-		{title: 'One', url: 'https://google.com'},
-		{title: 'Two', url: 'https://google.com'},
-	]
+	tagName: 'ul'
 });
--- app/routes/pins/index.js
+++ app/routes/pins/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function() {
+		return [
+			{title: 'Free Hug', url: 'https://google.com'},
+			{title: 'Free Hate', url: 'https://google.com'},
+		];
+	}
 });
