09b686540c8b46f5aae70bf540585e952c9093f0
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-30 18:21:06 -0400

    Upgraded ember and ember-data and added ember-parse and added in keys

--- config/environment.js
+++ config/environment.js
@@ -14,8 +14,19 @@ module.exports = function(environment) {
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+       applicationId: 'K6Qx64VypSGVsYuV7Hq6jtsxOi28WOsjdaniy2c9',
+       restApiId: '5Cgjkdbixow8vaJluTQ5Jv3a39rLpdigjzqy9cOi'
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' api.parse.com",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+     }		     
     }
   };
 
