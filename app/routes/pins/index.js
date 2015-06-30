146cb1f944fff8787da4dd4d2712bcd786efa7c5
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-30 18:48:19 -0400

    Have my data from parse

--- app/routes/pins/index.js
+++ app/routes/pins/index.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-		return [
-			{title: 'Free Hug', url: 'https://google.com'},
-			{title: 'Free Hate', url: 'https://google.com'},
-		];
+		return this.store.findAll('pin');
 	}
 });
