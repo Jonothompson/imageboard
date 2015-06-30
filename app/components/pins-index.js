140f0911b15cdc49ebfc43eb3b653cb7f6268acd
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-30 18:42:45 -0400

    Moved data to compnent

--- app/components/pins-index.js
+++ app/components/pins-index.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'ul'
+	tagName: 'ul',
+	
+	pins: [
+		{title: 'One', url: 'https://google.com'},
+		{title: 'Two', url: 'https://google.com'},
+	]
 });
