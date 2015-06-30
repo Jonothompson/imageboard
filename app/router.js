0fb0efb0515a7eb5724a4b1fce953b4b29f3df67
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-30 19:16:39 -0400

    have create function working but lost my display

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('pins', function() {});
+  this.route('pins', function() {
+    this.route('create');
+  });
 });
 
 export default Router;
